d40f8e3e7d1e9dc4f7263e5c90cc955d52aec890
julianaesteves <juliana.trindade@bandtec.com.br>
2022-04-25 00:00:03 -0300

    linkando login

--- projeto-react/onhome/src/components/Header/index.jsx
+++ projeto-react/onhome/src/components/Header/index.jsx
@@ -1,28 +1,34 @@
-import { Link } from 'react-scroll'
 import './style.css'
+import { Link } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll";
 
 const Header = () => {
     return (
         <div>
             <header className="container--mainContainerHeader">
-                    <div className="header--logo">
-                    <Link to='container--mainContainerHeader' smooth={true} duration={1000}><span>OnHome</span></Link>
-                    </div>
-                    <div className='header--options'>
-                        <nav>
-                            <ul>
-                                <li><Link to='solutions--container' smooth={true} duration={1000}>Serviços</Link></li>
-                                <li><Link to='products' smooth={true} duration={1000}>Produtos</Link></li>
+                <div className="header--logo">
+                    <LinkScroll to='container--mainContainerHeader' smooth={true} duration={1000}><span>OnHome</span></LinkScroll>
+                </div>
+                <div className='header--options'>
+                    <nav>
+                        <ul>
+                            <li><LinkScroll to='solutions--container' smooth={true} duration={1000}>Serviços</LinkScroll></li>
+                                <li><LinkScroll to='products' smooth={true} duration={1000}>Produtos</LinkScroll></li>
                                 <li>Sobre nós</li>
                                 <li>Planos</li>
                                 <li>Suporte</li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="header--login">
-                        <button className='btn--login'>Login</button>
-                        <button className='btn--registrar'>Registrar-se</button>
-                    </div>
+                         
+                        </ul>
+                    </nav>
+                </div>
+                <div className="header--login">
+
+
+                    <button className='btn--login'><Link to={"/login"}>Login</Link></button>
+
+
+                    <button className='btn--registrar'>Registrar-se</button>
+                </div>
             </header>
         </div>
     )
