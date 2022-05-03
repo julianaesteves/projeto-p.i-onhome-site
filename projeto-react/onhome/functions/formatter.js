e618168451ca2a7372d4a30fc09e3e9d3a829cc4
msutto <rafael_lopesferreira@hotmail.com>
2022-05-03 02:05:19 -0300

    formatação

---
+++ projeto-react/onhome/functions/formatter.js
@@ -0,0 +1,23 @@
+function formatarCpf(ipt) {
+    ipt.value = formatter(ipt.value, '###.###.###-##');
+}
+function formatarCep(ipt) {
+    ipt.value = formatter(ipt.value, '#####-###');
+}
+function formatter(value, mask) {
+    const text = value.replace(/\D/g, '');
+    const special = value.replace(/\d/g, '');
+    if (special[special.length - 1] === value[value.length - 1]) return value.substring(0, value.length - 1);
+    const size = text.length;
+    if (size > 0) {
+        let result = mask;
+        for (const character of text) {
+            result = result.replace('#', character);
+        }
+        const hash = result.indexOf('#');
+        if (hash === -1) {
+            return result;
+        }
+        return result.substring(0, hash);
+    }
+}
