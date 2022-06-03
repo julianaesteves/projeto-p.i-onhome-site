1f8eaaada180e691cf3dfb20dba97880e22cb074
julianaesteves <juliana.trindade@bandtec.com.br>
2022-06-03 12:29:05 -0300

    puxando dados do banco

--- projeto-react/onhome/src/pages/Dashboard/Components/UsersList/index.jsx
+++ projeto-react/onhome/src/pages/Dashboard/Components/UsersList/index.jsx
@@ -7,7 +7,6 @@ import EditModal from "../EditModal";
 import DeleteModal from "../DeleteModal";
 import { useEffect } from "react";
 
-// Receber os cadastrados no banco de dados
 const UsersList = () => {
   const [modal, setModal] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
@@ -17,22 +16,16 @@ const UsersList = () => {
   const [valueInputArea, setValueInputArea] = useState("");
   const [valueInputPermissao, setValueInputPermissao] = useState("");
 
-  const [usuarios, setUsuarios] = useState(null);
+  const [usuarios, setUsuarios] = useState();
 
-
-  useEffect(()=> {
-    fetch("http://localhost:8080/usuario/37")
-   
-    .then((res) => res.json())
-    .then(
-      (result) => {
-        setUsuarios(result)
-      },
-      (error) => {
-        console.error("error fetching data: ", error)
-      }
-    )
-  }, []) ;
+  useEffect(() => {
+    async function getUserInfo() {
+      const data = await fetch("http://localhost:8080/usuario/37");
+      const [json] = await data.json();
+      setUsuarios(json);
+    }
+    getUserInfo();
+  }, []);
 
   return (
     <>
@@ -56,41 +49,33 @@ const UsersList = () => {
       <div className="usuarios-gradient">
         <div className="border-gradient" style={{ padding: "20px" }}>
           <h2 className="cadastrados-title">Cadastrados</h2>
-          <div className="registered-users">
-            {usuarios && usuarios.map((post) => {
-              const {
-                idUsuario,
-                nomeUsuario,
-                emailUser,
-                senhaUser,
-                nomeEspecialidade,
-                cargo,
-              } = post
-              return (
-              <h2>{post.nomeUsuario}</h2>
-              )
-              })}
-            {/* {JSON.stringify(usuarios)} */}
-            <div className="mui-icons">
-              <EditIcon
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  setModal(true);
-                  setValueInputName("Felipe Camara");
-                  setValueInputEmail("felipe.camara@bandtec.com.br");
-                  setValueInputSenha("admin@admin");
-                  setValueInputArea("Front-end");
-                  setValueInputPermissao("Administrador");
-                }}
-              />
-              <DeleteIcon
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  setDeleteModal(true);
-                }}
-              />
-            </div>
-          </div>
+
+          {usuarios?.map((usuario) => 
+            (
+              <div className="registered-users">
+                {usuario.nomeUsuario}
+                <div className="mui-icons">
+                  <EditIcon
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      setModal(true);
+                      setValueInputName(`${usuario.nomeUsuario}`);
+                      setValueInputEmail(`${usuario.emailUser}`);
+                      setValueInputSenha(`${usuario.senhaUser}`);
+                      setValueInputArea(`${usuario.nomeEspecialidade}`);
+                      setValueInputPermissao(`${usuario.cargo}`);
+                    }}
+                  />
+                  <DeleteIcon
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      setDeleteModal(true);
+                    }}
+                  />
+                </div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </>
