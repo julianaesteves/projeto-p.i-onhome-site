dad37e4f6e915e47e6cab3fcd99f31e3fa658784
foliveiracamara <felipe.camara@bandtec.com.br>
2022-06-06 10:35:05 -0300

    fix

--- projeto-react/onhome/src/pages/Dashboard/Components/UsersList/index.jsx
+++ projeto-react/onhome/src/pages/Dashboard/Components/UsersList/index.jsx
@@ -21,7 +21,7 @@ const UsersList = () => {
   useEffect(() => {
     async function getUserInfo() {
 
-      const data = await fetch("https://onhome-api-v1.herokuapp.com//usuario/37");
+      const data = await fetch("https://onhome-api-v1.herokuapp.com/usuarios/37");
       const [json] = await data.json();
       setUsuarios(json);
     }
